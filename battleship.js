acafd27ac59855d3338074402b9273761ce4bf37
teguhpambudif <teguhpambudi1251@gmail.com>
2024-04-30 00:06:53 +0700

    refactor

--- battleship.js
+++ battleship.js
@@ -1,13 +1,14 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random()*5);
+var location1 = randomLoc;
+var location2 = location1+1;
+var location3 = location2+1;
 
 var guess;
 var hits = 0;
 var guesses = 0;
 
 var isSunk = false;
-var allowedGuess = [0,1,2,3,4,5,6]
+var allowedGuess = [0,1,2,3,4,5,6];
 
 while (isSunk == false) {
     guess = prompt("Ready, aim, Fire! (enter number between 0-6):");
@@ -26,14 +27,14 @@ while (isSunk == false) {
     if (guess == location1 || guess == location2 || guess == location3){
         hits += 1
         alert("HIT!")
+        if (hits == 3) {
+            isSunk = !isSunk
+            alert('Congratulations!! You sank my battleship!!')
+        }
     } else {
         alert("MISS!")
     }
     
-    if (hits == 3) {
-        isSunk = !isSunk
-        alert('Congratulations!! You sank my battleship!!')
-    }
 }
 var stats = `"You took ${guesses} shoots to sunk a battleships!"\n"which means you shoot accuracy was ${(3/guesses).toFixed(2)}"`;
 alert(stats)
